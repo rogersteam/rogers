60976bfc838e49effea3621c5419851e47d77a0f
rogersteam <mildredbailes139@gmail.com>
2024-05-08 11:27:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_5nwvymu", "template_1ve6sva", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
